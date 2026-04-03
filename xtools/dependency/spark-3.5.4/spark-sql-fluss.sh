9a0cb44323ee728ee1a976bd54405686af37d520
slwx <2622478542@qq.com>
2026-04-03 17:01:32 +0800

    -

---
+++ xtools/dependency/spark-3.5.4/spark-sql-fluss.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+spark-sql --master yarn --packages org.apache.fluss:fluss-spark-3.5_2.12:0.9.0-incubating,org.apache.fluss:fluss-fs-hdfs:0.9.0-incubating \
+  --conf spark.sql.catalog.fluss_catalog=org.apache.fluss.spark.SparkCatalog \
+  --conf spark.sql.catalog.fluss_catalog.bootstrap.servers=fluss:9123 \
+  --conf spark.sql.extensions=org.apache.fluss.spark.FlussSparkSessionExtensions \
+  -i ${SPARK_HOME}/conf/fluss_catalog_init.sql
